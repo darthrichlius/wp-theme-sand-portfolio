a7f6757aee11ce7916a0007efbb0377ebe713774
Lou Carther <dieudrichard@gmail.com>
2021-06-25 13:41:26 +0200

    UI adjustements

--- contents/content-contact.php
+++ contents/content-contact.php
@@ -1,14 +1,22 @@
 <div class="pg-ctct-main">
     <div>
         <h3 class="pg-ctct-tle">A project, a question?</h3>
-        <p class="pg-ctct-email">digitalwebexpert the-a-thing rdieud.com</p>
+        <p class="pg-ctct-email">
+            <span class="iconify" data-icon="line-md:email-opened-twotone" data-inline="false"></span>
+            digitalwebexpert [the-a-thing] rdieud.com
+        </p>
     </div>
     <div>
         <p class="pg-ctct-nota"><?= __("Due to a big number of email received, please think about reaching me through my professional social networks <span class='help-note'>(look at the bottom left of the page)</span> to make sure we don't miss each other") ?></p>
     </div>
     <div class="pg-ctct-spam-wrng">
         <h3 class="pg-ctct-spam-wrng-title">Spam Warning</h3>
-        <p class="pg-ctct-spam-wrng-par"><u>This email is only meant for business-related exchanges.</u> <br />Please don't use this email for marketing or any form of spamming activities. </p>
-        <p class="pg-ctct-spam-wrng-par">Transgressors will be blocked, banned and added onto an international spammer blacklist.</p>
+        <p class="pg-ctct-spam-wrng-par">
+            This email is only meant for business-related communication exchanges.<br />
+            Please don't use this email for marketing or any form of spamming activities.
+        </p>
+        <p class="pg-ctct-spam-wrng-par">
+            Transgressors will be blocked, banned and added onto an international spammer blacklist.
+        </p>
     </div>
 </div>
